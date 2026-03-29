f0d39721a7027737d7171f2040cd8518f037b4bc
tt2468 <tt2468@gmail.com>
2026-03-28 20:57:45 -0700

    Restart only if return code != 0

--- run.sh
+++ run.sh
@@ -2,23 +2,27 @@
 
 cd "$(dirname "$0")"
 
-if [ ! -f "./venv" ]; then
+if [ ! -d "./venv" ]; then
 	echo "./venv directory not found. Creating new one and installing dependencies"
 	/usr/bin/env python3 -m venv venv
 	venv/bin/pip install -r requirements.txt
 fi
 
 control_c() {
-    exit
+	exit
 }
 
 trap control_c SIGINT
 
 while true; do
-    venv/bin/python src/main.py
-    echo "Crash found."
-    for i in {5..1}; do
-        echo  "Restarting in $i"
-        sleep 1
-    done
+	venv/bin/python src/main.py
+	if [ $? -ne 0 ]; then
+		echo "Crash found."
+		for i in {5..1}; do
+			echo  "Restarting in $i"
+			sleep 1
+		done
+	else
+		exit 0
+	fi
 done
